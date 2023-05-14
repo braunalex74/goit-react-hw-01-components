ba6f25d6ad638972f249151c0b76e758c82941af
braunalex74 <braunalex74@googlemail.com>
2023-05-14 19:48:54 +0300

    DZ_1_add_style

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,30 +1,74 @@
 import React from 'react';
-import './Profile.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
   const { followers, views, likes } = stats;
 
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    <div
+      style={{
+        width: '400px', // Змініть ширину основної картки за необхідністю
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+      }}
+    >
+      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          style={{ width: '100px', height: '100px', borderRadius: '50%' }}
+        />
+        <p style={{ fontSize: '20px', fontWeight: 'bold' }}>{username}</p>
+        <p style={{ color: '#666', marginTop: '5px' }}>@{tag}</p>
+        <p style={{ color: '#999', marginTop: '5px' }}>{location}</p>
       </div>
 
-      <div className="stats">
-        <div className="stat-card">
-          <span className="stat-label">Followers:</span>
-          <span className="stat-value">{followers}</span>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div
+          style={{
+            flex: '1',
+            padding: '10px',
+            margin: '0 5px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+            textAlign: 'center',
+          }}
+        >
+          <span style={{ fontWeight: 'bold' }}>Followers:</span>
+          <br />
+          <span style={{ fontSize: '16px' }}>{followers}</span>
         </div>
-        <div className="stat-card">
-          <span className="stat-label">Views:</span>
-          <span className="stat-value">{views}</span>
+        <div
+          style={{
+            flex: '1',
+            padding: '10px',
+            margin: '0 5px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+            textAlign: 'center',
+          }}
+        >
+          <span style={{ fontWeight: 'bold' }}>Views:</span>
+          <br />
+          <span style={{ fontSize: '16px' }}>{views}</span>
         </div>
-        <div className="stat-card">
-          <span className="stat-label">Likes:</span>
-          <span className="stat-value">{likes}</span>
+        <div
+          style={{
+            flex: '1',
+            padding: '10px',
+            margin: '0 5px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+            textAlign: 'center',
+          }}
+        >
+          <span style={{ fontWeight: 'bold' }}>Likes:</span>
+          <br />
+          <span style={{ fontSize: '16px' }}>{likes}</span>
         </div>
       </div>
     </div>
