540befb71fcada33f86fd96f951597ee04fb6c41
braunalex74 <braunalex74@googlemail.com>
2023-05-16 22:47:11 +0300

    DZ_1_1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 // import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
